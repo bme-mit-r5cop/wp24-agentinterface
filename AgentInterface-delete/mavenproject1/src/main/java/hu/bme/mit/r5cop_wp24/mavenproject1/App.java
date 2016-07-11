41fe4e49f0fbecd672311bdb67a0c8dab9290863
eredics_peter <eredics.peter@gmail.com>
2016-07-11 18:57:06 +0200

    First ROS integartion tries

---
+++ AgentInterface-delete/mavenproject1/src/main/java/hu/bme/mit/r5cop_wp24/mavenproject1/App.java
@@ -0,0 +1,13 @@
+package hu.bme.mit.r5cop_wp24.mavenproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
